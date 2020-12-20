884c7811075e658c46a9863448e4cfdbfb8ecf26
Galina <titengal@gmail.com>
2020-12-20 22:30:44 +0300

    часть тестов

--- GraficReadactorDevEdu.Tests/FigureTests/CircleTests.cs
+++ GraficReadactorDevEdu.Tests/FigureTests/CircleTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Drawing;
 using System;
+using GraficReadactorDevEdu.Factor;
 using GraficReadactorDevEdu.Figure;
 
 namespace GraficReadactorDevEdu.Tests
@@ -12,19 +13,20 @@ namespace GraficReadactorDevEdu.Tests
         [Test]
 
         /*[TestCase(5, 5)]*/
-        public void GetPointsTest()
-        {  
+        public void UpdateTest()
+        {
             //arrange
-            var circle = new Circle();
+            var factory = new CircleFactory();
+            var circle = new Circle(factory);
             var startPoint = new Point(0, 0);
             var endPoint = new Point(2, 0);
             //act
-            var points = circle.GetPoints(startPoint, endPoint);
+           circle.Update(startPoint, endPoint);
             //assert        
 
 
-            Assert.AreEqual(startPoint, points[0]);
-            Assert.AreEqual(endPoint, points[1]);
+            Assert.AreEqual(startPoint, circle.Points[0]);
+            Assert.AreEqual(endPoint, circle.Points[1]);
 
 
         }
