b9ea512cae996140d2e7f4bc87754f85f1fbf9f7
Galina <titengal@gmail.com>
2020-12-21 18:33:23 +0300

    тесты+public class в двух фигурах

---
+++ GraficReadactorDevEdu.Tests/FigureTests/EllipseTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+using System;
+using GraficReadactorDevEdu.Factor;
+using GraficReadactorDevEdu.Figure;
+
+namespace GraficReadactorDevEdu.Tests.FigureTests
+{
+
+    [TestFixture]
+    public class EllipseTests
+    {
+        [Test]
+
+        public void UpdateTest()
+        {
+            //arrange
+            var factory = new EllipseFactory();
+            var ellipse = new Ellipse(factory);
+            var startPoint = new Point(0, 0);
+            var endPoint = new Point(2, 0);
+            //act
+            ellipse.Update(startPoint, endPoint);
+            //assert        
+
+            Assert.AreEqual(startPoint, ellipse.Points[0]);
+            Assert.AreEqual(endPoint, ellipse.Points[1]);
+
+
+        }
+    }
+}
---
+++ GraficReadactorDevEdu.Tests/FigureTests/FreeTriangleTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+using System;
+using GraficReadactorDevEdu.Factor;
+using GraficReadactorDevEdu.Figure;
+
+namespace GraficReadactorDevEdu.Tests.FigureTests
+{
+
+    [TestFixture]
+    public class FreeTriangleTests
+    {
+        [Test]
+
+        public void UpdateTest()
+        {
+            //arrange
+            var factory = new FreeTriangleFactory();
+            var freetriangle = new FreeTriangle(factory);
+            var startPoint = new Point(0, 0);
+            var endPoint = new Point(2, 0);
+            //act
+            freetriangle.Update(startPoint, endPoint);
+            //assert        
+
+            Assert.AreEqual(startPoint, freetriangle.Points[0]);
+            Assert.AreEqual(endPoint, freetriangle.Points[1]);
+
+
+        }
+    }
+}
